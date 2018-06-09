583946307242aaa325ac90d27b2a2c055455b748
Christian Scott <christianfscott@gmail.com>
2018-06-10 01:38:44 +1200

    format webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,38 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-    template: path.join(__dirname, "example/src/index.html"),
-    filename: "./index.html"
+  template: path.join(__dirname, "example/src/index.html"),
+  filename: "./index.html"
 });
+
 module.exports = {
-    entry: path.join(__dirname, "example/src/index.js"),
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: [
-                  {
-                  loader: "babel-loader",
-                  options: {
-                   presets: ['react'],
-                   plugins: ['transform-class-properties']
-                   }
-                }
-              ]
-            },
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
+  entry: path.join(__dirname, "example/src/index.js"),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["react"],
+              plugins: ["transform-class-properties"]
             }
+          }
         ]
-    },
-    plugins: [htmlWebpackPlugin],
-    resolve: {
-        extensions: [".js", ".jsx"]
-    },
-    devServer: {
-        port: 3001
-    }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    port: 3001
+  }
 };
