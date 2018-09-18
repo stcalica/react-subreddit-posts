c9de454fea9b1ae097672d6263b0fb078a25f12d
kyle.calica-steinhilber <kyle.calica-steinhilber@workday.com>
2018-09-18 10:22:41 -0700

    added two webpacks
    Change-Id: Ia6cd1e47edbabf07b2a008aa7600accbb1bc4872

---
+++ webpack.prod.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  entry: path.join(__dirname, "src"),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["react"],
+              plugins: ["transform-class-properties"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
+};
